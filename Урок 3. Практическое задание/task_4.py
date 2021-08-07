1449d583617bbbcece8d1542ba0656f579b3c98f
MonakhovEvgeny <79536395+MonakhovEvgeny@users.noreply.github.com>
2021-08-07 15:05:59 +0300

    Update task_4.py

--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -11,3 +11,31 @@
 
 Задание творческое. Здесь нет жестких требований к выполнению.
 """
+
+
+import random
+import string
+import hashlib
+
+cache = {}
+
+
+def random_string(length=20):
+    sequence_for_salt = string.ascii_letters + string.digits
+    salt = ''.join(random.sample(sequence_for_salt, length))
+    return salt
+
+
+def caching_web_pages(url):
+    if cache.get(url):
+        print(f'Данный адрес: {url} присутствует в кэше')
+    else:
+        res = hashlib.sha256(random_string().encode() + url.encode()).hexdigest()
+        cache[url] = res
+        print(cache)
+
+
+caching_web_pages('https://yandex.ru')
+caching_web_pages('https://yandex.ru')
+caching_web_pages('https://habr.com/ru')
+# random_string()
